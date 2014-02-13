0627885b4fbdf2aef9668655600cb3b68f60c8af
robajz <robajz@7e7ec879-d035-48bd-856f-8fbb7ac3437c>
2014-02-13 12:43:32 +0000

    Ad Issue #1 - renaming projects to allign with log4net convention.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,58 @@
+﻿#region Apache License
+//
+// Licensed to the Apache Software Foundation (ASF) under one or more 
+// contributor license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright ownership. 
+// The ASF licenses this file to you under the Apache License, Version 2.0
+// (the "License"); you may not use this file except in compliance with 
+// the License. You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#endregion
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("log4net.Json.Test.UI")]
+[assembly: AssemblyDescription("Tests to enable JSON logging in log4net")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Robert Sevcik")]
+[assembly: AssemblyProduct("log4net.Json.Test.UI")]
+[assembly: AssemblyCopyright("Copyright © Robert Sevcik 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fb06d805-f37b-4645-9cef-c223788d486d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.2.13.*")]
+[assembly: AssemblyFileVersion("1.2.13.0")]
+
+// configure the log4net framework
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
